fa47e4159f1983ef20c4fab1a1051157169b43be
Amar Pajarito <Amar Pajarito@AMAR-TUFGAMING>
2025-02-06 10:12:37 +0800

    new project

---
+++ src/main/java/com/mycompany/schoolequipmentsystemv2/SchoolEquipmentSystemv2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.schoolequipmentsystemv2;
+
+/**
+ *
+ * @author Amar Pajarito
+ */
+public class SchoolEquipmentSystemv2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
