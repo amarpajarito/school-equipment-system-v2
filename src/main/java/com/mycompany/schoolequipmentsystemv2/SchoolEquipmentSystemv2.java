6807b2b6425a1c68ff4fd5bbe8404a8ea5ce9002
Amar Pajarito <Amar Pajarito@AMAR-TUFGAMING>
2025-02-06 10:44:39 +0800

    modified main java

--- src/main/java/com/mycompany/schoolequipmentsystemv2/SchoolEquipmentSystemv2.java
+++ src/main/java/com/mycompany/schoolequipmentsystemv2/SchoolEquipmentSystemv2.java
@@ -4,6 +4,10 @@
 
 package com.mycompany.schoolequipmentsystemv2;
 
+import APP.*;
+import com.formdev.flatlaf.FlatLightLaf;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 /**
  *
  * @author Amar Pajarito
@@ -11,6 +15,14 @@ package com.mycompany.schoolequipmentsystemv2;
 public class SchoolEquipmentSystemv2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        try {
+            UIManager.setLookAndFeel(new FlatLightLaf());
+        } catch (UnsupportedLookAndFeelException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+
+        java.awt.EventQueue.invokeLater(() -> {
+            new Login().setVisible(true); 
+        });
     }
 }
